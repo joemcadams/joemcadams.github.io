59c2a212f42715474f6e122320912d3d106344dc
joemcadams <joseph.mcadams@mu.edu>
2017-02-03 11:49:25 -0600

    Updated styles so the home and about section looks cleaner on larger screens

--- src/js/components/HomeSection.js
+++ src/js/components/HomeSection.js
@@ -6,19 +6,21 @@ class HomeSection extends Component {
     render() {
         return (
             <div id="home-page">
-                <div id="home-name-banner">
-                    <h1> Joe McAdams </h1>
-                </div>
-                <div id="home-description">
-                    <p> Full Stack Developer </p>
-                    <p> DIY Enthusiast  </p>
-                    <p> Critically-Acclaimed Hipster </p>
-                    <a href="https://linkedin.com/in/josephwmcadams">
-                        <FaLinkedinSquare />
-                    </a>
-                    <a href="https://github.com/joemcadams">
-                        <FaGithubSquare />
-                    </a>
+                <div id="home-page-content">
+                    <div id="home-name-banner">
+                        <h1> Joe McAdams </h1>
+                    </div>
+                    <div id="home-description">
+                        <p> Full Stack Developer </p>
+                        <p> DIY Enthusiast  </p>
+                        <p> Critically-Acclaimed Hipster </p>
+                        <a href="https://linkedin.com/in/josephwmcadams">
+                            <FaLinkedinSquare />
+                        </a>
+                        <a href="https://github.com/joemcadams">
+                            <FaGithubSquare />
+                        </a>
+                    </div>
                 </div>
             </div>
         )
