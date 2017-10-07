4aa85f59353c4e62c98edc2fdefea8d787388a62
joemcadams <joseph.mcadams@mu.edu>
2017-10-07 14:01:05 -0500

    Added images directory, small cleanups elsewhere

--- src/js/components/ProjectItem.js
+++ src/js/components/ProjectItem.js
@@ -8,24 +8,24 @@ import FaGlobe from 'react-icons/lib/fa/globe'
 import GoCircuitBoard from 'react-icons/lib/go/circuit-board'
 import { PROJECT_CONTAINER_STYLES, PROJECT_ITEM_STYLES } from '../styles/ProjectItem'
 
-const projectTypes = {
+const PROJECT_TYPES = {
     DESKTOP: "desktop",
     WEB: "web",
     HARDWARE: "hardware",
     MOBILE: "mobile"
 }
 
-const ProjectItem = (props) => {
+const ProjectItem = props => {
     
     let allTools = ""
     props.tools.forEach((tool, i) => { i === props.tools.length - 1 ? allTools += `${tool}` : allTools += `${tool}, ` })
     
     let icon = {}
     const iconSize = 30
-    if (props.type === projectTypes.DESKTOP) icon = <FaDesktop size={ iconSize }/>
-    if (props.type === projectTypes.HARDWARE) icon = <GoCircuitBoard size={ iconSize }/>
-    if (props.type === projectTypes.MOBILE) icon = <FaMobile size={ iconSize }/>
-    if (props.type === projectTypes.WEB) icon = <FaGlobe size={ iconSize }/>
+    if (props.type === PROJECT_TYPES.DESKTOP) icon = <FaDesktop size={ iconSize }/>
+    if (props.type === PROJECT_TYPES.HARDWARE) icon = <GoCircuitBoard size={ iconSize }/>
+    if (props.type === PROJECT_TYPES.MOBILE) icon = <FaMobile size={ iconSize }/>
+    if (props.type === PROJECT_TYPES.WEB) icon = <FaGlobe size={ iconSize }/>
     // <CardMedia><img src={ props.imagePath } alt="Project Image" /></CardMedia>
     
     return (
