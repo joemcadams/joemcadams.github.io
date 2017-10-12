d5e5875de5e09ba2189122c8228755918a324005
joemcadams <joseph.mcadams@mu.edu>
2017-10-12 13:42:54 -0500

    updated bootstrap import, removed unnecessary npm modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css' // To use reactstrap in the app
 import './styles/styles.css'
 import App from './js/App'
 
