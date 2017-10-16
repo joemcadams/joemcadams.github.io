f4b01e38dfe01df23ac133499caea772eeb3b350
joemcadams <joseph.mcadams@mu.edu>
2017-10-16 11:05:53 -0500

    Updates

--- src/js/styles/NavigationBar.js
+++
@@ -1,6 +0,0 @@
-import { blueGrey50 } from 'material-ui/styles/colors'
-
-export const CONTENT_STYLES = {
-    padding: window.innerWidth > 800 ? '5vh 5vw 5vh 5vw' : '5vh 1vw 5vh 1vw',
-    backgroundColor: blueGrey50
-}
